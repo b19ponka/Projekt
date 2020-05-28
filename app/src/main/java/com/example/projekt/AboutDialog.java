ab2224d01b172a55a707f8b7edaeaaae23892a68
pontu <b19ponka@student.his.se>
2020-05-28 16:10:26 +0200

    Added text and changed app name.

--- app/src/main/java/com/example/projekt/AboutDialog.java
+++ app/src/main/java/com/example/projekt/AboutDialog.java
@@ -12,7 +12,10 @@ public class AboutDialog extends AppCompatDialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Om appen")
-                .setMessage("text")
+                .setMessage("Denna appen är till för dig som letar efter den perfekta " +
+                        "för just dig. Här kan du enkelt och smidigt läsa om olika hundraser. " +
+                        "För tillfället finns bara en handfull raser med begränsad information " +
+                        "i appen då den är i ett väldigt tidigt stadie.")
                 .setPositiveButton("STÄNG", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
