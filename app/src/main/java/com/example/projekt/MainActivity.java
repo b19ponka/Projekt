ffca3663fe72d312c90d13a0211fe0c49d47efc0
pontu <b19ponka@student.his.se>
2020-05-28 15:20:00 +0200

    Added two member variables, an ArrayList named items and a ArrayAdapter named adapter. I create them empty and initiate them in the onCreate method. Then, with the JsonTask, i convert the JSON data from the webb service into string or int values that i store in the items variable. I then connect the adapter to the listview and add the items. Lastly i make a onItemClick method that creates a string, message, made up of text and the values fetched by the getters made in the Breed class.

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+
+    private static final String TAG = "MainActivity";
+
+    ArrayList<Breed> items;
+    ArrayAdapter<Breed> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +47,89 @@ public class MainActivity extends AppCompatActivity {
                 openDialog();
             }
         });
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, R.layout.listview_item, items);
+
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Breed breed = items.get(i);
+                String message = "Rasen " + breed.getName() +
+                        " är en " + breed.getCompany() +
+                        " och blir som vuxen omkring " + breed.getSize() + " cm i mankhöjd";
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=b19ponka");
     }
 
     public void openDialog() {
         AboutDialog aboutDialog = new AboutDialog();
         aboutDialog.show(getSupportFragmentManager(), "about dialog");
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            try {
+                items.clear();
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String name = jsonObject.getString("name");
+                    String location = jsonObject.getString("company");
+                    int height = jsonObject.getInt("size");
+                    Breed breed = new Breed(name, location, height);
+                    items.add(breed);
+                }
+                adapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                Log.d(TAG, "Could not parse json: " + json + "\n due to exception: " + e);
+            }
+        }
     }
 }
